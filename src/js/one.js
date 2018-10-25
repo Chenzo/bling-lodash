d61bad9073dc009cb76a6bcdc07991a8fb7e7377
Vince Suriani <chenzorama@hotmail.com>
2018-10-25 08:49:10 -0400

    Before meld

--- src/js/one.js
+++ src/js/one.js
@@ -1,3 +1,40 @@
 
+import './modules/bling.js';
+
 
 console.log("One JS loaded and Run");
+
+console.log($);
+
+var container = document.querySelector("#box");
+console.log(container);
+
+
+var boxContainer = $("#box")[0];
+console.log(boxContainer);
+
+
+
+boxContainer.classList.add("red-font");
+boxContainer.on("click", function() {
+	console.log("boop");
+});
+
+
+var getMe = $(".js-getme")[0];
+console.log(getMe);
+
+getMe.style.height = '200px';
+getMe.style.width = '200px';
+getMe.style.border = '2px solid black'; 
+
+
+
+var allDivs = $(".box-style");
+
+allDivs.forEach(function(element) {
+	console.log(element);
+	element.style.margin = "40px";
+});
+
+
--- www/index.php
+++ www/index.php
@@ -19,6 +19,16 @@
             <h1>Mind Meld 10.25.2018</h1>
             <h2>Bling Dot JS | LoDash</h2>
 
+
+            <article>
+
+                <div class="box-style">I'm just a box</div>
+                <div id="box" class="box-style">I'm #box</div>
+                <div class="js-getme box-style">I'm .js-getme</div>
+
+
+            </article>
+
         </section>
 
         <script async="async" type="application/javascript" src="/js/one.js"></script>
